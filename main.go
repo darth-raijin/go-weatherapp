e6a3868a366300a0f6ba313bdd03f48b53485017
macow-lab <mmacovv@gmail.com>
2022-01-06 21:15:25 +0100

    Progress on navbat comp

--- main.go
+++ main.go
@@ -20,7 +20,7 @@ func logging(f http.HandlerFunc) http.HandlerFunc {
 func test_fragment(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("templates/fragments/header.html"))
 	log.Println("yessir")
-	tmpl.Execute(w, "aa")
+	tmpl.Execute(w, "s")
 }
 
 func root(w http.ResponseWriter, r *http.Request) {
@@ -32,15 +32,13 @@ func view_city(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
-	// Adding Tailwind CSS compatibility
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 	mux.Handle("/css/", twhandler.New(http.Dir("css"), "/css", twembed.New()))
 
-	http.HandleFunc("/", logging(root))
-	http.HandleFunc("/city", logging(view_city))
-	http.HandleFunc("/t", logging(test_fragment))
+	mux.HandleFunc("/test", test_fragment)
 
-	http.ListenAndServe(":8080", nil)
-	log.Println("Weather application is good to Go!")
+	http.ListenAndServe(":8080", mux)
+	fmt.Println("viewing specific city")
+
 }
